7c8f3cc64b1434d52821b1817bfb53f117588dbf
mtjkl <matej.kalavsky@gmail.com>
2025-01-01 21:42:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,7 +31,8 @@ if ingredients_list: #is not null by default
     
     for fruits_chosen in ingredients_list:
         ingredients_string += fruits_chosen + ' '
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        st.subheader(fruit_chosen + ' Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
         #st.text(smoothiefroot_response.json())
         st_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width = True)
 
